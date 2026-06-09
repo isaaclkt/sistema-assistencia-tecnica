16b9e7cd59ea1718561b44c3e43e80e7542ea270
isaaclkt <isaaclk08@gmail.com>
2026-06-09 11:31:18 -0300

    Implementa cadastro e listagem de peças no estoque

--- routes/estoque.py
+++ routes/estoque.py
@@ -1,7 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from database import conectar
 
 estoque_bp = Blueprint('estoque', __name__)
 
 @estoque_bp.route('/estoque')
 def listar_estoque():
-    return render_template('estoque.html')
+    db = conectar()
+
+    pecas = db.execute("""
+        SELECT *
+        FROM pecas
+        ORDER BY nome
+    """).fetchall()
+
+    db.close()
+
+    return render_template('estoque.html', pecas=pecas)
+
+
+@estoque_bp.route('/estoque/cadastrar', methods=['POST'])
+def cadastrar_peca():
+    nome = request.form['nome']
+    descricao = request.form['descricao']
+    quantidade = request.form['quantidade']
+    estoque_minimo = request.form['estoque_minimo']
+    preco_unitario = request.form['preco_unitario']
+    fornecedor = request.form['fornecedor']
+
+    db = conectar()
+
+    db.execute("""
+        INSERT INTO pecas 
+        (nome, descricao, quantidade, estoque_minimo, preco_unitario, fornecedor)
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, (nome, descricao, quantidade, estoque_minimo, preco_unitario, fornecedor))
+
+    db.commit()
+    db.close()
+
+    return redirect('/estoque')
