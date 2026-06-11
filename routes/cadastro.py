58fdbf81878eb8380945f62c332ae9e05e569e72
arthurmedeiros0 <arthurdinizazevedo@gmail.com>
2026-06-11 08:40:49 -0300

    Atualizei o código

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -25,23 +25,18 @@ def cadastrar_cliente():
     conexao = conectar()
     cursor = conexao.cursor()
 
-    cursor.execute("""
-        INSERT INTO clientes
-        (nome, cpf, telefone, email, endereco)
-
-        VALUES (?, ?, ?, ?, ?)
-    """, (nome, cpf, telefone, email, endereco))
+    cursor.execute("""INSERT INTO clientes (nome, cpf, telefone, email, endereco) VALUES (?, ?, ?, ?, ?)""", (nome, cpf, telefone, email, endereco))
 
     conexao.commit()
     conexao.close()
 
     return redirect("/cadastro")
 
+# READ
 @cadastro_bp.route("/cadastro")
 def listar_cadastros():
 
     conexao = conectar()
-    conexao.row_factory = sqlite3.Row
     cursor = conexao.cursor()
 
     cursor.execute("SELECT * FROM clientes")
@@ -49,17 +44,13 @@ def listar_cadastros():
 
     conexao.close()
 
-    return render_template(
-        "gerenciar_clientes.html",
-        clientes=clientes
-    )
+    return render_template("gerenciar_clientes.html", clientes=clientes)
 
 
 # Página editar cliente
 @cadastro_bp.route("/clientes/atualizar/<int:id>", methods=["GET"])
 def pagina_atualizar_cliente(id):
     conexao = conectar()
-    conexao.row_factory = sqlite3.Row
     cursor = conexao.cursor()
 
     cursor.execute("SELECT * FROM clientes WHERE id = ?", (id,))
@@ -82,11 +73,7 @@ def atualizar_cliente(id):
     conexao = conectar()
     cursor = conexao.cursor()
 
-    cursor.execute("""
-        UPDATE clientes
-        SET nome = ?, cpf = ?, telefone = ?, email = ?, endereco = ?
-        WHERE id = ?
-    """, (nome, cpf, telefone, email, endereco, id))
+    cursor.execute("""UPDATE clientes SET nome = ?, cpf = ?, telefone = ?, email = ?, endereco = ? WHERE id = ?""", (nome, cpf, telefone, email, endereco, id))
 
     conexao.commit()
     conexao.close()
