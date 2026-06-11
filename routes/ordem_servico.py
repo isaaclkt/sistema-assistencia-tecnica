7cf53d55c571282722541c1aaf89585109990d8d
Lucas Buzzi Lima <lucas.buzzlima@gmail.com>
2026-06-10 22:52:27 -0300

    mostrar as ordens de serviço do banco de dados

--- routes/ordem_servico.py
+++ routes/ordem_servico.py
@@ -1,7 +1,17 @@
 from flask import Blueprint, render_template
+from database import conectar
 
 ordem_servico_bp = Blueprint("ordem_servico", __name__)
 
+# Página com as ordens de serviço
 @ordem_servico_bp.route("/ordem-servico")
 def listar_ordens():
-    return render_template("ordem-servico.html")
+    db = conectar()
+
+    ordens = db.execute("""
+        SELECT * FROM ordens_servico
+    """).fetchall()
+
+    db.close()
+
+    return render_template("ordem-servico.html", ordens=ordens)
